6091dca9e7d4085d1084cfdf9ddd6d2b9f2b12a6
arthur <fatih@qq.com>
2023-05-16 15:21:00 +0800

    update

--- readme.md
+++ readme.md
@@ -1,2 +1,8 @@
 
 https://blog.csdn.net/bandaoyu/article/details/116062334
+
+
+
+软件模拟
+
+https://github.com/zrlio/softiwarp
--- src/version.h
+++ src/version.h
@@ -6,4 +6,4 @@
 #define HDS_VERSION_MINOR 0
 #define HDS_VERSION_BUILD 317
 #define HDS_VERSION_FLAG  "beta"
-#define HDS_BUILD_TIME    "2023/04/07 15:59:26"
+#define HDS_BUILD_TIME    "2023/05/16 14:18:55"
